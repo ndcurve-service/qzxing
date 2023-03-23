a4882bbe6b389e9f752ff832fe30e55fd5e4a915
Muneer <elendilm@gmail.com>
2023-03-23 17:16:47 +0530

    build script for android-arm, android-arm64 and linux-x64

--- qzxing-build.sh
+++ qzxing-build.sh
@@ -1,8 +1,42 @@
 
 
- mkdir -p build && cd build && \
- ~/.Installation/Qt/5.15.2/gcc_64/bin/qmake ../qzxing.pro "CONFIG+=release" && \
- make
+
+ QT_VERSION=6.5.0
+ ANDROID_NDK="/home/muneer/.Installation/android-sdk-linux/ndk/25.1.8937393"
 
 
+ rm -rf build
+ rm -rf libs
 
+OS=$(uname)
+
+if [ $OS == "Linux"  ]; then PORTABLE_SED="sed -i ";
+    # Linux x64
+    mkdir -p build/linux-x64 && cd build/linux-x64 && \
+    ~/.Installation/Qt/$QT_VERSION/gcc_64/bin/qmake ../../qzxing.pro "CONFIG+=release" && \
+    make
+    cd ../../
+    mkdir -p libs/linux-x64 && cp build/linux-x64/libQZXing.a libs/linux-x64/libQZXing.a
+
+
+    # Android
+    export ANDROID_NDK_ROOT=$ANDROID_NDK
+
+    # Android arm64
+    mkdir -p build/android-arm64 && cd build/android-arm64 && \
+    ~/.Installation/Qt/$QT_VERSION/android_arm64_v8a/bin/qmake ../../qzxing.pro "CONFIG+=release" && \
+    make
+    cd ../../
+    mkdir -p libs/android-arm64 && cp build/android-arm64/libQZXing_arm64-v8a.so libs/android-arm64/libQZXing_arm64-v8a.so
+
+    # Android arm
+    mkdir -p build/android-arm && cd build/android-arm && \
+    ~/.Installation/Qt/$QT_VERSION/android_armv7/bin/qmake ../../qzxing.pro "CONFIG+=release" && \
+    make
+    cd ../../
+    mkdir -p libs/android-arm && cp build/android-arm/libQZXing_armeabi-v7a.so libs/android-arm/libQZXing_armeabi-v7a.so
+fi
+
+if [ $OS == "Darwin" ]; then
+
+fi
